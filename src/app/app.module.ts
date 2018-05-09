c10e4bb8b9f8820f1ea39007e13bd694c75da4f8
HenryHamamji <hamamji@stanford.edu>
2018-05-09 00:42:54 -0600

    dealing with backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses.component';
@@ -13,6 +13,10 @@ import { LikeComponent } from './like/like.component';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
+import { PostsComponent } from './posts/posts.component';
+import { HttpModule } from '@angular/http';
+
 
 @NgModule({
   declarations: [
@@ -27,11 +31,15 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
     ZippyComponent,
     ContactFormComponent,
     SignupFormComponent,
+    NewCourseFormComponent,
+    PostsComponent,
 
   ],
   imports: [
     BrowserModule,
-    FormsModule 
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule 
   ],
   providers: [
     CoursesService
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UsernameValidators } from './username.validators';
 
 @Component({
   selector: 'signup-form',
@@ -8,7 +9,32 @@ import { FormGroup, FormControl} from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
+   /* username: new FormControl('',[
+      Validators.required,
+      Validators.minLength(3),
+      UsernameValidators.cannotContainSpace
+      ],
+      UsernameValidators.shouldBeUnique
+),
+    password: new FormControl('', Validators.required), */
+    account: new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
+    })
   });
+
+  login(){
+    this.form.setErrors({
+      invalidLogin : true
+    });
+  }
+
+  /*get username(){
+    return this.form.get('username');
+  } */
+
+get username(){
+    return this.form.get('account.username');
+  } 
+
 }
